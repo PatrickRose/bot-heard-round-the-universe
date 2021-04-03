721ec10095b442932e7f3b7cb023f627254e10bb
Patrick Rose <pjr0911025@googlemail.com>
2021-04-03 18:48:47 +0100

    Make sure we import the dotenv config earlier

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { config } from 'dotenv';
+
+config(); // Recommended way of loading dotenv
+
 import container from './inversify.config';
 import { TYPES } from './types';
 import { Bot } from './bot';
-
-config(); // Recommended way of loading dotenv
 
 const bot = container.get<Bot>(TYPES.Bot);
 bot.listen()
